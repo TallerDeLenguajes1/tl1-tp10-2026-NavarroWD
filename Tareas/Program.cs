e6115ff6f0609cd9e3b349fdaf5e78fee55dea62
Navarro Walter <navarrowalterd@gmail.com>
2026-06-28 03:27:42 -0300

    punto 2 usuarios

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+HttpClient Cliente = new HttpClient();
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+HttpResponseMessage respuesta = await Cliente.GetAsync(url);
+respuesta.EnsureSuccessStatusCode();
+
+string Json = await respuesta.Content.ReadAsStringAsync();
+
+List<Tarea> listTarea = JsonSerializer.Deserialize<List<Tarea>>(Json);
+
+foreach (var tarea in listTarea){
+    Console.WriteLine("UserID: " + tarea.userId + " ID: " + tarea.id + " Titulo: " + tarea.title + " Completado: " + tarea.completed);
+}
+
+Console.WriteLine("/// Tareas Completadas ///");
+
+foreach(var tareaComp in listTarea)
+{
+    if(tareaComp.completed == true)
+    {
+        {
+            Console.WriteLine("UserID: " + tareaComp.userId + " ID: " + tareaComp.id + " Titulo: " + tareaComp.title + " Completado: " + tareaComp.completed);
+        }
+    }
+}
+
+System.Console.WriteLine("/// Tareas Pendientes ///");
+
+foreach(var tareaPend in listTarea)
+{
+    if(tareaPend.completed == false)
+    {
+        {
+            Console.WriteLine("UserID: " + tareaPend.userId + " ID: " + tareaPend.id + " Titulo: " + tareaPend.title + " Completado: " + tareaPend.completed);
+        }
+    }
+}
+
+string jsonString = JsonSerializer.Serialize(listTarea);
+File.WriteAllText("tareas.json", jsonString);
+System.Console.WriteLine("Archivo Json creado exitosamente.");
+
+
+
+public class Tarea
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public bool completed { get; set; }
+
+}
---
+++ Usuarios/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+var url = "https://jsonplaceholder.typicode.com/users";
+HttpClient client = new HttpClient();
+HttpResponseMessage respuesta = await client.GetAsync(url);
+respuesta.EnsureSuccessStatusCode();
+string respuestaTexto = await respuesta.Content.ReadAsStringAsync();
+List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(respuestaTexto);
+
+if(usuarios == null)
+{
+    System.Console.WriteLine("Fallo al obtener usuarios. ");
+    return;
+}
+
+int contador = 0;
+
+foreach(var usuario in usuarios)
+{
+    if(contador < 5)
+    {
+        System.Console.WriteLine("Nombre: " + usuario.name + " // " + " Email: " + usuario.email + " Domicilio: " + usuario.address.street + " " + usuario.address.suite + ", " + usuario.address.city + ", " + usuario.address.zipcode + ". \n");
+    }
+    else
+    {
+        break;
+    }
+contador++;
+}
+
+string jsonString = JsonSerializer.Serialize(usuarios);
+File.WriteAllText("Usuarios.json", jsonString);
+System.Console.WriteLine("Archivo Json creado exitosamente. ");
+
+public class Usuario
+{
+    public int id {get; set;}
+    public string name {get; set;}
+    public string username {get; set;}
+    public string email {get; set;}
+    public Address address {get; set;} = new Address();
+    public string phone {get; set;}
+    public string website {get; set;}
+    public Company company {get; set;} = new Company();
+    
+}
+
+public class Address
+{
+    public string street {get; set;}
+    public string suite {get; set;}
+    public string city {get; set;}
+    public string zipcode {get; set;}
+    public Geo geo {get; set;} = new Geo();
+}
+
+public class Geo
+{
+    public string lat {get; set;}
+    public string lng {get; set;}
+}
+
+public class Company
+{
+    public string name {get; set;}
+    public string catchPhrase {get; set;}
+    public string bs {get; set;}
+}
